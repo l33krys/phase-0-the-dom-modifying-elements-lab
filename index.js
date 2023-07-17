625a2770a302ddb67484c023e3d26df9c11558df
Krystle Lee <l33.krys@gmail.com>
2023-07-16 23:09:10 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.className = 'victory';
+newHeader.innerHTML = 'Krystle is the champion';
+newHeader.id = 'victory';
